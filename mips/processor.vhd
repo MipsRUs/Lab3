a347b33989b95ca9e480a46a12ee4ef7ceab811d
Ting-Yi Huang <tingyih@uci.edu>
2016-02-10 11:57:28 -0800

    changed ref_clk to clk in processor

--- mips/processor.vhd
+++ mips/processor.vhd
@@ -26,7 +26,7 @@ use ieee.numeric_std.all;
 
 ENTITY processor IS
 	PORT (
-		ref_clk : IN std_logic ;
+		clk : IN std_logic ;
 		reset : IN std_logic
 	);
 END processor;
@@ -451,7 +451,7 @@ signal checkpositive_out: std_logic_vector(31 DOWNTO 0);
 ------------------- begin --------------------- 
 begin
 
-	pcx:			pc PORT MAP(clk=>ref_clk, rst=>reset, addr_in=>JumpMux2PC, addr_out=>PCOut);	
+	pcx:			pc PORT MAP(clk=>clk, rst=>reset, addr_in=>JumpMux2PC, addr_out=>PCOut);	
 	
 	adder1x:		adder32 PORT MAP(a_32=>PCOut, b_32=>adder_b_32, cin=>adder1_cin, sub=>adder1_sub, 
 								sum_32=>adder1x_out, cout=>adder1_cout, ov=>adder1_ov);
@@ -475,7 +475,7 @@ begin
 								ALUControl=>ALUControl_out, rs=>rs_out, rt=>rt_out, rd=>rd_out, imm=>imm_out, shamt=>shamt_out,
 								jumpshiftleft=>jumpshiftleft_out);
 
-	regfilex:		regfile PORT MAP(clk=>ref_clk, rst_s=>reset, we=>RegWrite_out, raddr_1=>rs_out, raddr_2=>rt_out, waddr=>JALaddrMux2Waddr,
+	regfilex:		regfile PORT MAP(clk=>clk, rst_s=>reset, we=>RegWrite_out, raddr_1=>rs_out, raddr_2=>rt_out, waddr=>JALaddrMux2Waddr,
 								rdata_1=>rdata_1_out, rdata_2=>rdata_2_out, wdata=>JalDataMux2Reg);
 
 	SignExtensionImmx: sign_extension_16bit PORT MAP(immediate=>imm_out, sign_extension_out=>SignExtensionImm_out);
@@ -503,7 +503,7 @@ begin
 
 	JRControlMuxx: 	mux PORT MAP(in0=>concatination_out, in1=>rdata_1_out, sel=>JRControl_out, outb=>JRControlMux_out);
 
-	ramx:			ram PORT MAP(clk=>ref_clk, we=>MemWrite_out, addr=>checkpositive_out, dataI=>rdata_2_out, dataO=>mem_data_out);
+	ramx:			ram PORT MAP(clk=>clk, we=>MemWrite_out, addr=>checkpositive_out, dataI=>rdata_2_out, dataO=>mem_data_out);
 
 	checkpositivex: checkpositive PORT MAP(in0=>alu_out, outb=>checkpositive_out);
 
