adc231f42dbeeb793753acd72fbc24e72b47ef00
Stefan Cao <caostefan@gmail.com>
2016-02-13 09:14:31 -0800

    regfile

--- addersynth/design/regfile.vhd
+++ addersynth/design/regfile.vhd
@@ -1,65 +1,72 @@
+-------------------------------------------------------------------
+-- Copyright MIPS_R_US 2016 - All Rights Reserved 
+--
+-- File: ram.vhd
+-- Team: MIPS_R_US
+-- Members:
+-- 		Stefan Cao (ID# 79267250)
+--		Ting-Yi Huang (ID# 58106363)
+--		Nehme Saikali (ID# 89201494)
+--		Linda Vang (ID# 71434490)
+--
+-- Description:
+--		This is a RAM
+--
+-- History:
+-- 		Date		Update Description			Developer
+--	-----------   ----------------------   	  -------------
+--	1/16/2016		Created						TH, NS, LV, SC
+--	1/27/2016		Updating to With/Select		LV
+--	1/31/2016		Modified to byte addressable	LV
+--
+-------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 ENTITY regfile IS
-GENERIC ( NBIT : INTEGER := 32;
-NSEL : INTEGER := 5);
-PORT (
-clk : IN std_logic ;
-rst_s : IN std_logic ; -- synchronous reset
-we : IN std_logic ; -- write enable
-raddr_1 : IN std_logic_vector ( NSEL -1 DOWNTO 0); -- read address 1
-raddr_2 : IN std_logic_vector ( NSEL -1 DOWNTO 0); -- read address 2
-waddr : IN std_logic_vector ( NSEL -1 DOWNTO 0); -- write address
-rdata_1 : OUT std_logic_vector ( NBIT -1 DOWNTO 0); -- read data 1
-rdata_2 : OUT std_logic_vector ( NBIT -1 DOWNTO 0); -- read data 2
-wdata : IN std_logic_vector ( NBIT -1 DOWNTO 0) -- write data 1
-);
+
+	PORT (
+		ref_clk : IN std_logic ;
+		rst_s : IN std_logic ; 
+		we : IN std_logic ; -- write enable
+		raddr_1 : IN std_logic_vector (4 DOWNTO 0); -- read address 1
+		raddr_2 : IN std_logic_vector (4 DOWNTO 0); -- read address 2
+		waddr : IN std_logic_vector (4 DOWNTO 0); -- write address
+		rdata_1 : OUT std_logic_vector (31 DOWNTO 0); -- read data 1
+		rdata_2 : OUT std_logic_vector (31 DOWNTO 0); -- read data 2
+		wdata : IN std_logic_vector (31 DOWNTO 0) -- write data 1
+	);
 END regfile ;
 
-architecture beh of regfile is
---type memory is array (natural range <>, natural range <>) of std_logic;
---signal mem1: memory(2**NSEL-1 downto 0, NBIT-1 downto 0);
+architecture behavior of regfile is
 
-subtype word is std_logic_vector(NBIT-1 downto 0);
-type regFileTyp is array(0 to 2**NSEL-1) of word;
---signal reg: regFileType;
+subtype word is std_logic_vector(31 downto 0);
+type memory is array(0 to 2**4) of word;
 
+begin
+        ram_process: process (ref_clk, we, raddr_1, raddr_2 waddr, wdata)
+        variable mem_var:memory;
 
+        begin
 
-begin  -- beh
-  funct: process(clk)
-    variable mem1:regFileTyp;
-    variable RDATA1_VAR, RDATA2_VAR : STD_LOGIC_VECTOR(NBIT-1 downto 0);
-    variable zeros : std_logic_vector (NBIT-1 downto 0) := (others => '0');
-    begin
-    if(clk'event and clk='1' and rst_s='1') then
-     all_zero: for i in 0 to 2**NSEL-1 loop
-        mem1(i) := (others=>'0');
-      end loop;  -- i
-      rdata1_var := (others =>'0');
-      rdata2_var := (others =>'0');
-    elsif (clk'event and clk='1') then
-	if(to_integer(unsigned(raddr_1)) = 0) then
-		rdata1_var := zeros;
-	else
-		rdata1_var := mem1(to_integer(unsigned(raddr_1)));
-	end if;
-	if(to_integer(unsigned(raddr_2)) = 0) then
-		rdata2_var := zeros;
-	else
-		rdata2_var := mem1(to_integer(unsigned(raddr_2)));
-	end if;
-      if (we='1') then
-	if (to_integer(unsigned(waddr)) = 0) then
-	
-	else
-		mem1(to_integer(unsigned(waddr))) := wdata;
-	end if;
-      end if;
-    end if;
-    rdata_1 <= rdata1_var;
-    rdata_2 <=rdata2_var;
-    end process;
-end beh;
+        if(ref_clk'event and ref_clk='0') then
+                if(we='1') then
+                        mem_var(to_integer(unsigned(waddr))) := wdata;
+                else 
+                        rdata1 <= mem_var(to_integer(unsigned(raddr_1)));
+                        rdata2 <= mem_var(to_integer(unsigned(raddr_2)));
+                end if;
+                        
+        else 
+                if(we='1') then
+                        mem_var(to_integer(unsigned(waddr))) := wdata;
+                else
+                        rdata1 <= mem_var(to_integer(unsigned(raddr_1)));
+                        rdata2 <= mem_var(to_integer(unsigned(raddr_2)));
+                end if;
+        end if;
+
+        end process;
+end behavior;
